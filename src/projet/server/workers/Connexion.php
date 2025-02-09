781aecfd07b75c620d4526d0702496f0559ea32d
Simon <smauron07@gmail.com>
2025-02-09 19:03:34 +0100

    projet

--- src/projet/server/workers/Connexion.php
+++ src/projet/server/workers/Connexion.php
@@ -1,7 +1,5 @@
 <?php
 
-include_once('configConnexion.php');
-
 /**
  * Classe connexion
  *
@@ -33,7 +31,7 @@ class Connexion {
      */
     private function __construct() {
         try {
-            $this->pdo = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS, array(
+            $this->pdo = new PDO('mysql:host=localhost;dbname=DBNAME', 'root', 'root', array(
                 PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
                 PDO::ATTR_PERSISTENT => true));
         } catch (PDOException $e) {
