bbfcd6a2245c0c0723ca843c35019df12fd2a063
Simon Mauron <smauron07@gmail.com>
2025-02-11 11:44:02 +0100

    projet

--- src/projet/client/js/controllers/NonAuthentifieCtrl.js
+++ src/projet/client/js/controllers/NonAuthentifieCtrl.js
@@ -2,17 +2,17 @@
 class NonAuthentifieCtrl {
 
     constructor() {
-        http.chargerEquipe(this.creationListe.bind(this), this.gestionErreur.bind(this));
+        http.chargerEquipe(this.creationListeEquipe.bind(this), this.gestionErreurEquipe.bind(this));
+        
     }
 
     connect() {
 
     }
 
-    creationListe(data) {
-        let select = $("cmbEquipe");
-        select.empty(); // Vider les anciennes options
-
+    creationListeEquipe(data) {
+        let select = $("#cmbEquipe");
+        select.empty();
         $(data).find("equipe").each(function () {
             let id = $(this).find("pk_equipe").text();
             let nom = $(this).find("nom").text();
@@ -21,7 +21,22 @@ class NonAuthentifieCtrl {
     }
 
     
-    gestionErreur(xhr, status, error) {
+    gestionErreurEquipe(xhr, status, error) {
+        console.error("Erreur lors du chargement des équipes :", status, error);
+        alert("Une erreur est survenue lors du chargement des équipes. Veuillez réessayer plus tard.");
+    }
+
+    creationListeJoueur(data){
+        let select = $("#cmbJoueur");
+        select.empty();
+        $(data).find("joueur").each(function () {
+            let id = $(this).find("pk_equipe").text();
+            let nom = $(this).find("nom").text();
+            select.append(`<option value="${id}">${nom}</option>`);
+        });
+    }
+
+    gestionErreurJoueur(xhr, status, error) {
         console.error("Erreur lors du chargement des équipes :", status, error);
         alert("Une erreur est survenue lors du chargement des équipes. Veuillez réessayer plus tard.");
     }
